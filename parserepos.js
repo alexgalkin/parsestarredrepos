93afaa5e16367b66b51b4738525c39a971b50379
Alex Galkin <alex.galkin@geeksforless.com>
2017-05-12 12:44:18 +0300

    added missing dependency; added placeholder for API & DB parameters

--- parserepos.js
+++ parserepos.js
@@ -16,20 +16,29 @@ var github = new GitHubApi({
 
 github.authenticate({
     type: "oauth",
-    key: CLIENT_ID,
-    secret: CLIENT_SECRET
+    key: 'CLIENT_ID',
+    secret: 'CLIENT_SECRET'
 });
 
-// TODO: optional authentication here depending on desired endpoints. See below in README.
-
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var date = today.getFullYear() + '-' + ((today.getMonth()+1) < 10)?'0':'' + (today.getMonth()+1) + '-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+
+var Db = require('mongodb').Db,
+    Server = require('mongodb').Server,
+    MongoClient = require('mongodb').MongoClient;
+
+
+function addDateTime (element, index, array) {
+    array[index].date = date;
+    array[index].time = time;
+}
 
 var getDataForPage = function (pageNumber, result) {
 
   github.activity.getStarredReposForUser({
-  username: "alexgalkin",
+  username: "GITHUB_USERNAME",
   page: pageNumber,
   per_page: 100
   }, function(err, res) {
@@ -38,9 +47,31 @@ var getDataForPage = function (pageNumber, result) {
     fs.writeFile("/Users/alexg/projects/nikmobile/parsestarredrepos/" + date + "-" + time + pageNumber + ".json", JSON.stringify(res), function(err) {
     if(err) {
         return console.log(err);
+    } else {
+        console.log('storing results in mongodb...');
+
+        MongoClient.connect("mongodb://MONGO_USER:MONGO_PASS@MONGO_HOST:MONGO_PORT/MONGO_DB", function(err, db) {
+            if (err) {
+                console.log(JSON.stringify(err));
+            }
+           var collection = db.collection("repostats");
+
+            var arrayOfRepoData = res.data;
+
+            arrayOfRepoData.forEach(addDateTime);
+
+            collection.insertMany(arrayOfRepoData, null, function() {
+                console.log('saving data: done');
+                db.close();
+            });
+
+
+
+
+        });
     }
 
-    console.log("The file was saved!");
+        console.log('saving file: done');
     });
 
     if (github.hasNextPage(res)) {
